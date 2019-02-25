0cbdc12edf9aaf0960b166636ffd7bfbbfa7a34b
Ezran Bayantemur <ezranbayantemur@gmail.com>
2019-02-25 20:10:08 +0300

    Model classes has been added.

---
+++ Bank.Models/Customers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BankApp.Model
+{
+    public class Customers : IDisposable
+    {
+	    public Dispose()
+	    {
+            GC.SuppressFinalize(this);
+	    }
+
+        public int CustomerID { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerSurname { get; set; }
+
+        public string CustomerPasskey { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public byte BalanceType { get; set; }
+
+        public bool isActive { get; set; }
+
+
+
+    }
+}
---
+++ Bank.Models/Transactions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BankApp.Model
+{ 
+    public class Transactions : IDisposable
+    {
+	    public Dispose()
+	    {
+            GC.SuppressFinalize(this);
+	    }
+
+        public int TransactionID { get; set; }
+ 
+        public decimal TransactionAmount { get; set; }
+
+        public int TransactorAccountNumber { get; set; }
+
+        public int? RecieverAccountNumber { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+
+        public bool isSuccess { get; set; }
+
+    }
+}
