4a9918c0126a2bb68b1f7e5edce00624df384eb3
Cengiz Cebeci <thejengo0o@gmail.com>
2019-02-25 19:57:04 +0300

    Converted git repo file to a .net solution started to dataAccess layer

---
+++ Bank.DataAccess/Concretes/CustomersRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Bank.DataAccess;
+using Bank.DataAccess.Abstractions;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace Bank.DataAccess.Concretes
+{
+    public class CustomersRepository : IRepository<Class1> IDisposable
+    {
+        public CustomersRepository()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        public bool DeletedById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(Class1 entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Class1> SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Class1 SelectedById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Class1 entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
